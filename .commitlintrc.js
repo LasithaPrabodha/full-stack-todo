715b0a4d29a03dfb03a2e80c74ea6e440d0b0c32
Lasitha Weligampola <lasiprabo@gmail.com>
2024-03-13 16:01:40 -0400

    fix(nx-cloud): fixed format check in nx cloud

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,8 +1,3 @@
-// const {
-//   utils: { getProjects },
-// } = require('@commitlint/config-nx-scopes');
-
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  
 };
--- libs/shared/util-testing/src/lib/todo.factory.ts
+++ libs/shared/util-testing/src/lib/todo.factory.ts
@@ -3,11 +3,12 @@ import { randBoolean, randProduct } from '@ngneat/falso';
 
 export const createMockTodo = (data?: Partial<ITodo>): ITodo => {
   const { id, title, description } = randProduct();
-  const todo: ITodo = Object.assign({}, data, {
+  const todo: ITodo = {
+    ...data,
     id,
     title,
     description,
     completed: randBoolean(),
-  });
+  };
   return todo;
 };
